3b152a36bdd0ecae59a4d18c58c8c7e61f27888b
rio <rio.game018@gmail.com>
2025-11-22 22:19:29 +0900

    next.config.mjs追加

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true
+  },
+  basePath: '/ChronosOfficial-if',
+  assetPrefix: '/ChronosOfficial-if',
+}
+
+export default nextConfig
